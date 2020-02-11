08175293c5a5f819b47cb3df3b0c0260447a48a5
kdluu <khanhluu101@gmail.com>
2020-02-11 09:41:53 -0600

    use localStorage to store count value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import Counter from './Counter';
+import { useIntersect } from './custom-hooks'
 
 const App = () => {
+  const [ref, entry] = useIntersect({});
+  console.log(entry);
   return (
     <div>
       <div className="counter-container">
-        <h1>React Hook Dojo</h1>
-
+        <h1 >React Hook Dojo</h1>
         <Counter />
       </div>
 
       <div className="out-of-view">
-        <h2>Hello World</h2>
+        <h2 ref={ref}>Hello World</h2>
       </div>
     </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -1,52 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Counter.css';
 
-class Counter extends React.Component {
-  constructor() {
-    super();
+const Counter = () => {
+  // Every time it render, instead of take the intitial count to 0, we going to look into the localStorage to see if there any 'count' value, otherwise we set it to 0
+  // This purpose of making the initialCount is a function is it only call once when we render the page the first time, prevent it call multiple time when we try to access the intialCount.
+  const initialCount = () => Number(window.localStorage.getItem('count') || 0)
+  const [count, setCount] = useState(initialCount); // <---intialCount
+  // This useEffect callback after every single time our component rerender.
+  // However, we only want useEffect to be call when the count value changes specificly
+  // useEffect hooks allow us to passing an array as a second args, React will only run when the count value changes.
+  useEffect(() => {
+    localStorage.setItem('count', count)
+  }, [count])
+  const subtract = () => {
+    setCount(count - 1)
 
-    this.state = {
-      count: 0,
-    };
   }
-
-  subtract = () => {
-    this.setState((state) => ({
-      count: state.count - 1,
-    }));
+  const add = () => {
+    setCount(count + 1)
   }
+  return (
+    <div className="counter">
+      <h2>{count}</h2>
 
-  add = () => {
-    this.setState((state) => ({
-      count: state.count + 1,
-    }));
-  }
-
-  render() {
-    return (
-      <div className="counter">
-        <h2>{this.state.count}</h2>
-
-        <div className="counter-buttons">
-          <button
-            className="counter-button"
-            data-test="subtract-button"
-            onClick={this.subtract}
-          >
-            Sub
+      <div className="counter-buttons">
+        <button
+          className="counter-button"
+          data-test="subtract-button"
+          // onClick={() => setCount(count - 1)}
+          onClick={subtract}
+        >
+          Sub
           </button>
 
-          <button
-            className="counter-button"
-            data-test="add-button"
-            onClick={this.add}
-          >
-            Add
+        <button
+          className="counter-button"
+          data-test="add-button"
+          // onClick={() => setCount(count + 1)}
+          onClick={add}
+        >
+          Add
           </button>
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
+
 }
+
 
 export default Counter;
